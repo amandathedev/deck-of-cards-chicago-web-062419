1f25bc32cac773239f211fdccb10a4bc9e345119
Amanda Treutler <amandathedev@gmail.com>
2019-07-10 10:14:16 -0500

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,34 @@
-class Deck
+require "pry"
 
+class Deck
+  attr_reader :cards
+
+  def cards
+    Card.deck
+  end
+
+  def choose_card
+    Deck.cards.pop
+  end
+
+  def self.deck
+    @@deck = []
+    @@rank_a.each do |rank|
+      @@suit_a.each do |suit|
+        card = Card.new
+        card.suit = suit
+        card.rank = rank
+        @@deck << card
+      end
+    end
+    @@deck
+  end
 end
 
 class Card
+  attr_accessor :rank, :suit
+  attr_reader :suit_a, :rank_a
 
+  @@rank_a = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  @@suit_a = ["Hearts", "Clubs", "Diamonds", "Spades"]
 end
